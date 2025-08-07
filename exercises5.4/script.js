ab94b6e18688ba81327505544410342b928145be
The Great White Rabbit <gribomen@mail.ru>
2025-08-07 23:06:15 +0300

    Я сделал урок 5.4 Оператор switch case

---
+++ exercises5.4/script.js
@@ -0,0 +1,40 @@
+const role = 'menager';
+
+switch(role){
+    case 'menager':
+            console.log('Менеджер');
+            break;
+    case 'admin':
+            console.log('Админ');
+            break;
+    case 'ceo':
+        console.log('CEO');
+        break;
+    default:
+        console.log('Мы тебя не знаем');
+}
+
+switch(role){
+    case 'menager':
+    case 'admin':
+            console.log('Не руководитель');
+            break;
+    case 'ceo':
+        console.log('Руководитель');
+        break;
+    default:
+        console.log('Мы тебя не знаем');
+}
+
+const num = 1;
+
+switch(true){
+    case num > 0:
+        console.log('Положительный');
+        break;
+    case num < 0:
+        console.log('Отрицательный');
+        break;
+    default:
+        console.log('Ноль!');
+}
