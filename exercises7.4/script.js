b9ef4f6617f15a710f4db96fc1fe06b0fc0dae07
The White Rabbit <gribomen@mail.ru>
2025-08-15 16:54:17 +0300

    Выполнил домашнее задание по вставке коммита

---
+++ exercises7.4/script.js
@@ -0,0 +1,9 @@
+function toPower(num, power){
+    const res = num ** power;
+    return res;
+}
+
+console.log(toPower(2,3));
+
+const toPowerArrow = (num,power)=>num**power;
+console.log(toPowerArrow(2,3));
