d53f9ff5b03271a2d7c89be25f81cb8791f1dcdc
The White Rabbit <gribomen@mail.ru>
2025-08-27 18:51:23 +0300

    Добавил практику к уроку знакомство с массивом

---
+++ exercises8.1/script.js
@@ -0,0 +1,9 @@
+const role1 = 'admin';
+const role2 = 'user';
+const role3 = 'superuser';
+
+const roles = ['admin','user','superuser'];
+const userInfo = ['Аня', 25];
+console.log(roles);
+console.log(roles[roles.length-1]);
+console.log(roles.at(-1));
