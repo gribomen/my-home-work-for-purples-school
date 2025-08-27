cd08cd3c19eba39e41770d092f01a318b810052c
The White Rabbit <gribomen@mail.ru>
2025-08-27 19:33:51 +0300

    Еще один урок по массивам, точнее удобному поиску

---
+++ exercises8.3/script.js
@@ -0,0 +1,17 @@
+const roles = ['user', 'admin', 'manager'];
+
+const elIndex = roles.indexOf('admin');
+console.log(elIndex);
+const elIndex2 = roles.indexOf('superuser');
+console.log(elIndex2);
+
+if(roles.indexOf('admin')>=0){
+    console.log('Доступ есть');
+}
+
+console.log(roles.includes('admin'));
+console.log(roles.includes('superuser'));
+
+if(roles.includes('admin')){
+    console.log('Доступ есть');
+}
