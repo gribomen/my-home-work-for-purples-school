4cdee2c7047d01faee1eeaae21507e38d98def2e
The Great White Rabbit <gribomen@mail.ru>
2025-09-10 15:55:44 +0300

    Финалочка)

--- 9-sort-loops/script.js
+++ 9-sort-loops/script.js
@@ -5,7 +5,7 @@ console.log(sort(arr));
 
 function sort(arr){
     for(let i = 0 ; i < arr.length; i++){
-        for(let j = 0 ; i < arr.length; j++){
+        for(let j = i ; j < arr.length; j++){
             if(arr[i] < arr[j]){
                 const change = arr[i];
                 arr[i] = arr[j];
