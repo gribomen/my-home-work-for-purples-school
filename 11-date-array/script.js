496df79c07acaeea21eb2d29c7cd7f5789917182
The White Rabbit <gribomen@mail.ru>
2025-09-11 22:17:06 +0300

    Да каюсь бот нашел у меня в коде ошибку, бот меня умнее(((((

--- 11-date-array/script.js
+++ 11-date-array/script.js
@@ -1,14 +1,13 @@
-const arr = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023', '15.05.2012'];
+const arr = ['10-02-2022', 'тест', '1j/1g/2f23', '00/13/2022', '41/12/2023', '15.05.2012'];
 
 function processingArrays(arr){
     return arr.filter(element => element.length === 10)
     .map(element=> {
         const new_arr = element.slice("");
         return [[new_arr[0]+new_arr[1]],[new_arr[3] + new_arr[4]],[new_arr[6] + new_arr[7] + new_arr[8] + new_arr[9]]];
-
     }).filter( element =>{
 
-        if(!(typeof Number(element[0]) == "number" && Number(element[0]) < 31 && Number(element[0]) > 0)){
+        if(!(typeof Number(element[0]) == "number" && Number(element[0]) < 32 && Number(element[0]) > 0)){
             return false;
         }
 
