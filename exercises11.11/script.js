0d2ba28d0d8c962225180abff4993537b80b643b
The White Rabbit <gribomen@mail.ru>
2025-09-11 20:56:04 +0300

    Изучил класс Array, точнее некторые его возможности

---
+++ exercises11.11/script.js
@@ -0,0 +1,10 @@
+const arr = [1,2,3,4,5];
+console.log(new Array(1,2,3,4,5));
+const arr2 = new Array(5);
+console.log(arr2);
+arr2.fill(1,0,3);
+arr2.fill(2,3,5);
+console.log(arr2);
+
+const arr3 = Array.from({length:5},(cur,i) => i++);
+console.log(arr3);
