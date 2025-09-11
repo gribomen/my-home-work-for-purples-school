0e2845a7880d4b0d6b7296c0b13e9dacd28f263a
The White Rabbit <gribomen@mail.ru>
2025-09-11 17:07:34 +0300

    Внес последние изменения в скрипты

--- exercises11.4/script.js
+++ exercises11.4/script.js
@@ -1,4 +1,5 @@
 const price = [[100,200],[120,100],[200,350]];
 
-console.log(price.filter(item => (item[1]-item[0]) > 0 )
-    .map(item => (item[1]-item[0])));
+console.log(price.map(item => item[1]-item[0])
+    .filter(item => item > 0 ));
+    
