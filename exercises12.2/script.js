7abcaf3f7f2a72746699d1ea30795ce293453089
The White Rabbit <gribomen@mail.ru>
2025-09-12 10:30:27 +0300

    Проработал методы работы со строками

---
+++ exercises12.2/script.js
@@ -0,0 +1,9 @@
+const userName = '🧮Вася Пупкин';
+console.log(userName[0]);
+console.log(userName[0] + userName[1]);
+console.log(userName.charAt(2));
+
+console.log(userName.indexOf('Пу'));
+console.log(userName.lastIndexOf('а'));
+console.log(userName.includes('а'));
+console.log(userName.slice(7));
