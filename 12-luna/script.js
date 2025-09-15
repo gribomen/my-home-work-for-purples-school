2040b8511a59da820f0db9afb0a4365cc5ac9603
The White Rabbit <gribomen@mail.ru>
2025-09-15 17:00:28 +0300

    Подправил логику

--- 12-luna/script.js
+++ 12-luna/script.js
@@ -1,16 +1,16 @@
 const card = '4561-2612-1234-5464';
 console.log(luna(card));
 function luna(card){
-    return card.split("-").reduce((acc,element)=> acc+= element.split("").reduce((acc2,item)=> {
+    return card.split("-").reduce((acc,element)=> acc+= element.split("").reduce((acc2,item,index)=> {
         let num = Number(item);
-        if((num%2)===0){
+        if((index%2)===0){
             if((num*2)>9){
                 return acc2 + (num * 2)-9;
             }else{
                 return acc2= acc2 + (num*2);
             }
         }
-        if((num%2) === 1){
+        if((index%2) === 1){
             return acc2= acc2 + num;           
         }
     },0),0) % 10 === 0;
